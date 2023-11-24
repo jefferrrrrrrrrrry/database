6731d22789995cc13e99b74f09b59fa6fc82243f
Your Name <2358931408@qq.com>
2023-11-24 20:48:32 +0800

    springboot

--- buaadb/src/main/java/com/example/buaadb/BuaadbApplication.java
+++ buaadb/src/main/java/com/example/buaadb/BuaadbApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.buaadb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class BuaadbApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(BuaadbApplication.class, args);
     }
 
 }
--- buaadb/src/test/java/com/example/buaadb/BuaadbApplicationTests.java
+++ buaadb/src/test/java/com/example/buaadb/BuaadbApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.buaadb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class BuaadbApplicationTests {
 
     @Test
     void contextLoads() {
