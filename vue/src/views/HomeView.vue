5d1e009783cfe4bb8ae3dab7b4ddf32ced008972
Your Name <2358931408@qq.com>
2023-11-27 15:25:04 +0800

    vue

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="home" style="height:100%">
     <el-container style="height: 100%; border: 1px solid #eee">
-  <el-aside width="sideWidth +'px'" style="background-color: rgb(238, 241, 246); height: 100% overflow: hidden;">
-    <el-menu :default-openeds="['1', '3']" style="height:100%" background-color="rgb(0,0,0)" 
-              text-color="#fff" active-text-color="#ffd04b" :collapse-transition="false" :collapse="isCollapse">
-      <el-submenu index="1">
+  <el-aside width="sideWidth +'px'" style="background-color: rgb(238, 241, 246); height: 100% overflow: hidden; box-shadow:2px 0 6px rgba(0,21,41,0.35)">
+    <el-menu :default-openeds="['1', '3']" style="height:100%" background-color="rgb(135,206,235)" 
+              text-color="#000080" active-text-color="#fff" :collapse-transition="false" :collapse="isCollapse">
+       <div style="height:60px; line-height:60px; text-align:center" v-show="collapse">
+        <img src="../assets/buaa.png" alt="" style="width:30px; vertical-align:middle;"> 
+        <b style="color:rgb(0,0,128); vertical-align:middle;"> BUAA选课系统</b>
+      </div>
+        <el-submenu index="1">
         <template slot="title">
           <i class="el-icon-message"></i>
           <span>导航一</span>
@@ -14,14 +18,7 @@
           <el-menu-item index="1-1">选项1</el-menu-item>
           <el-menu-item index="1-2">选项2</el-menu-item>
         </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="1-4-1">选项4-1</el-menu-item>
         </el-submenu>
-      </el-submenu>
       <el-submenu index="2">
         <template slot="title">
           <i class="el-icon-menu"></i>
@@ -32,32 +29,7 @@
           <el-menu-item index="2-1">选项1</el-menu-item>
           <el-menu-item index="2-2">选项2</el-menu-item>
         </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="2-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="2-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="2-4-1">选项4-1</el-menu-item>
         </el-submenu>
-      </el-submenu>
-      <el-submenu index="3">
-        <template slot="title">
-          <i class="el-icon-setting"></i>
-          <span>导航一</span>
-        </template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="3-1">选项1</el-menu-item>
-          <el-menu-item index="3-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="3-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="3-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
     </el-menu>
   </el-aside>
   
@@ -78,16 +50,24 @@
     </el-header>
     
     <el-main>
+      <el-breadcrumb separator="/">
+  <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+  <el-breadcrumb-item><a href="/">已选课程</a></el-breadcrumb-item>
+</el-breadcrumb>
+      <div style="padding:10px 0;display:flex">
+        <el-input style="flex:1 width:200px"  placeholder="请输入课程名" suffix-icon="el-icon-search"></el-input>
+        <el-button style="margin-left:5px " type="primary">搜索</el-button>
+      </div>
       <el-table :data="tableData">
         <el-table-column prop="number" label="序号" width="60" >
         </el-table-column>
-        <el-table-column prop="id" label="课程代码" width="120">
+        <el-table-column prop="id" label="课程代码" width="100">
         </el-table-column>
-        <el-table-column prop="name" label="课程名称" width="120">
+        <el-table-column prop="name" label="课程名称" width="100">
         </el-table-column>
-        <el-table-column prop="address" label="校区" width="120">
+        <el-table-column prop="address" label="校区" width="100">
         </el-table-column>
-        <el-table-column prop="information" label="上课信息" width="450">
+        <el-table-column prop="information" label="上课信息" width="420">
           <template slot-scope="scope">
             <!-- 使用 v-html 渲染包含换行标签的字符串 -->
             <span v-html="formatText(scope.row.information)"></span>
@@ -95,19 +75,37 @@
         </el-table-column>
         <el-table-column prop="form" label="课程类别" width="100">
         </el-table-column>
-        <el-table-column prop="nature" label="课程性质" width="130">
-        </el-table-column>
-        <el-table-column prop="department" label="开课院系" width="130">
+        <el-table-column prop="department" label="开课院系" width="100">
         </el-table-column>
         <el-table-column prop="credits" label="学分" width="70">
         </el-table-column>
-        <el-table-column prop="remaining" label="剩余人数/容量" >
+        <el-table-column prop="remaining" label="剩余人数/容量" width="120">
           <template slot-scope="scope">
             <!-- 使用 v-html 渲染包含换行标签的字符串 -->
             <span v-html="formatText(scope.row.remaining)"></span>
           </template>
         </el-table-column>
+          <el-table-column label="操作">
+      <template slot-scope="scope">
+        <el-button
+          size="mini"
+          type="danger"
+          @click="handleDelete(scope.$index, scope.row)">退选</el-button>
+      </template>
+    </el-table-column>  
+          
       </el-table>
+      <div style="padding:10px 0"> 
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage4"
+          :page-sizes="[5,10,15,20]"
+          :page-size="15"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="400">
+        </el-pagination>
+      </div>
     </el-main>
   </el-container>
 </el-container>
@@ -131,7 +129,7 @@ export default {
         nature:'核心专业类',
         department:'计算机学院',
         credits:'4.5',
-        remaining:'容量:117/120\n',
+        remaining:'117/120',
     };
     return {
       tableData: Array(10).fill(item),
@@ -150,6 +148,11 @@ export default {
     this.isCollapse=!this.isCollapse;
     if(this.isCollapse){
       this.sideWidth=64;
+    }
+    if(this.collapseBtnClass=='el-icon-s-fold'){
+      this.collapseBtnClass='el-icon-s-unfold'
+    }else{
+      this.collapseBtnClass='el-icon-s-fold'
     }
   }
 },
