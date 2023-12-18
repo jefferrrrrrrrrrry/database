c31caa1ed38bb98a4390bcfe621878e2f3bf9945
徐志衡 <21371109@buaa.edu.cn>
2023-12-18 23:55:47 +0800

    dfbh

---
+++ buaadb/src/main/java/com/example/buaadb/controller/SelController.java
@@ -0,0 +1,35 @@
+package com.example.buaadb.controller;
+
+import com.example.buaadb.common.Result;
+import com.example.buaadb.entity.Course;
+import com.example.buaadb.entity.Sel;
+import com.example.buaadb.entity.Student;
+import com.example.buaadb.mapper.SelMapper;
+import com.example.buaadb.service.SelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/sel")
+public class SelController {
+    @Autowired
+    private SelMapper selMapper;
+    @Autowired
+    private SelService selService;
+
+    @PostMapping("/selectCourse")
+    public Result selectCourse(@RequestBody Student student,
+                               @RequestBody Course course){
+        return Result.success();
+    }
+
+    @PostMapping("/add")
+    public Result add(@RequestBody Sel sel) {
+        return Result.success(selService.save(sel));
+    }
+
+    @DeleteMapping ("/del")
+    public Result del(@RequestBody Sel sel) {
+        return Result.success(selService.removeById(sel));
+    }
+}
