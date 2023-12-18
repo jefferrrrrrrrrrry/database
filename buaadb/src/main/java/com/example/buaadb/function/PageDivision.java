4501d0a12479e2732b686caba4bb487124bbc878
1 <21371202@buaa.edu.cn>
2023-12-18 23:54:20 +0800

    页面处理更新

--- buaadb/src/main/java/com/example/buaadb/function/PageDivision.java
+++ buaadb/src/main/java/com/example/buaadb/function/PageDivision.java
@@ -4,16 +4,19 @@ import com.example.buaadb.common.Result;
 import com.example.buaadb.entity.output.CourseInfo;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 public class PageDivision {
     public static <T> HashMap<String, Object> getPage(List<T> list, Integer pageNum, Integer pageSize) {
         int total = list.size();
         int totalPage = (total / pageSize) + ((total % pageSize > 0) ? 1 : 0);
+        List<T> page;
         if (pageNum > totalPage || pageNum <= 0) {
-            return null;
+            page = new LinkedList<>();
+        } else {
+            page = list.subList((pageNum - 1) * pageSize, Math.min(pageNum * pageSize, total));
         }
-        List<T> page = list.subList((pageNum - 1) * pageSize, Math.min(pageNum * pageSize, total));
         HashMap<String, Object> res = new HashMap<>();
         res.put("total", total);
         res.put("totalPage", totalPage);
