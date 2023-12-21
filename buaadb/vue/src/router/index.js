378bb533632f38805e81bb65a817ee82f1e58f35
Your Name <2358931408@qq.com>
2023-12-21 21:50:33 +0800

    课程

--- buaadb/vue/src/router/index.js
+++ buaadb/vue/src/router/index.js
@@ -1,35 +1,41 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import StudentView from '../views/StudentView.vue'
 import ManagerView from '../views/ManagerView.vue'
+import StudentCourseChosenView from "@/views/StudentCourseChosenView.vue";
+import StudentView from "@/views/StudentView.vue";
+import StudentCourseView from "@/views/StudentCourseView.vue";
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/student',
-    name: 'student',
-    component: StudentView
-  },
-  {
-    path: '/manager',
-    name: 'manager',
-    component: ManagerView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    {
+        path: '/student',
+        name: 'student',
+        component: StudentView,
+        children: [
+            {path: "courseChosen", name: "studentCourseChosen", component: StudentCourseChosenView},
+            {path: "course", name: "studentCourse", component: StudentCourseView}
+        ]
+    },
+    {
+        path: '/manager',
+        name: 'manager',
+        component: ManagerView
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
