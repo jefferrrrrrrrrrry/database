24f61cf3e4e39933aaf8e19897f8fad6711b1fff
1 <21371202@buaa.edu.cn>
2023-12-24 01:15:04 +0800

    12345

--- buaadb/src/main/java/com/example/buaadb/common/interceptor/JwtInterceptor.java
+++ buaadb/src/main/java/com/example/buaadb/common/interceptor/JwtInterceptor.java
@@ -1,51 +1,51 @@
-package com.example.buaadb.common.interceptor;
-
-import cn.hutool.core.util.StrUtil;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTDecodeException;
-import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.example.buaadb.common.Status;
-import com.example.buaadb.entity.User;
-import com.example.buaadb.exception.ServiceException;
-import com.example.buaadb.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.method.HandlerMethod;
-import org.springframework.web.servlet.HandlerInterceptor;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public class JwtInterceptor implements HandlerInterceptor {
-    @Autowired
-    private UserService userService;
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String token = request.getHeader("token");
-        if (!(handler instanceof HandlerMethod)) {
-            return true;
-        }
-        if (StrUtil.isBlank(token)) {
-            throw new ServiceException(Status.ERROR, "未获取到token信息");
-        }
-        String userId;
-        try {
-            userId = JWT.decode(token).getAudience().get(0);
-        } catch (JWTDecodeException j) {
-            throw new ServiceException(Status.ERROR, "token验证失败");
-        }
-        User user = userService.getById(userId);
-        if (user == null) {
-            throw new ServiceException(Status.ERROR, "用户不存在");
-        }
-        JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(user.getSys_password())).build();
-        try {
-            jwtVerifier.verify(token);
-        } catch (JWTVerificationException e) {
-            throw new ServiceException(Status.ERROR, "token验证失败，请重新登录");
-        }
-        return true;
-    }
-}
+//package com.example.buaadb.common.interceptor;
+//
+//import cn.hutool.core.util.StrUtil;
+//import com.auth0.jwt.JWT;
+//import com.auth0.jwt.JWTVerifier;
+//import com.auth0.jwt.algorithms.Algorithm;
+//import com.auth0.jwt.exceptions.JWTDecodeException;
+//import com.auth0.jwt.exceptions.JWTVerificationException;
+//import com.example.buaadb.common.Status;
+//import com.example.buaadb.entity.User;
+//import com.example.buaadb.exception.ServiceException;
+//import com.example.buaadb.service.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.method.HandlerMethod;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//public class JwtInterceptor implements HandlerInterceptor {
+//    @Autowired
+//    private UserService userService;
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        String token = request.getHeader("token");
+//        if (!(handler instanceof HandlerMethod)) {
+//            return true;
+//        }
+//        if (StrUtil.isBlank(token)) {
+//            throw new ServiceException(Status.ERROR, "未获取到token信息");
+//        }
+//        String userId;
+//        try {
+//            userId = JWT.decode(token).getAudience().get(0);
+//        } catch (JWTDecodeException j) {
+//            throw new ServiceException(Status.ERROR, "token验证失败");
+//        }
+//        User user = userService.getById(userId);
+//        if (user == null) {
+//            throw new ServiceException(Status.ERROR, "用户不存在");
+//        }
+//        JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(user.getSys_password())).build();
+//        try {
+//            jwtVerifier.verify(token);
+//        } catch (JWTVerificationException e) {
+//            throw new ServiceException(Status.ERROR, "token验证失败，请重新登录");
+//        }
+//        return true;
+//    }
+//}
--- buaadb/src/main/java/com/example/buaadb/config/InterceptorConfig.java
+++ buaadb/src/main/java/com/example/buaadb/config/InterceptorConfig.java
@@ -1,21 +1,21 @@
-package com.example.buaadb.config;
-
-import com.example.buaadb.common.interceptor.JwtInterceptor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class InterceptorConfig implements WebMvcConfigurer {
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(jwtInterceptor())
-                .addPathPatterns("/*")
-                .excludePathPatterns("/login");
-    }
-    @Bean
-    public JwtInterceptor jwtInterceptor() {
-        return new JwtInterceptor();
-    }
-}
+//package com.example.buaadb.config;
+//
+//import com.example.buaadb.common.interceptor.JwtInterceptor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class InterceptorConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(jwtInterceptor())
+//                .addPathPatterns("/*")
+//                .excludePathPatterns("/login");
+//    }
+//    @Bean
+//    public JwtInterceptor jwtInterceptor() {
+//        return new JwtInterceptor();
+//    }
+//}
