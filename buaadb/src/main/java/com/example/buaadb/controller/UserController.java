cba873f5e502e073329d02bac5a92d476dbca160
1 <21371202@buaa.edu.cn>
2023-12-24 20:19:56 +0800

    123123

--- buaadb/src/main/java/com/example/buaadb/controller/UserController.java
+++ buaadb/src/main/java/com/example/buaadb/controller/UserController.java
@@ -5,8 +5,11 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.buaadb.common.Result;
 import com.example.buaadb.common.Status;
 import com.example.buaadb.controller.logInfo.LogInfo;
+import com.example.buaadb.entity.Login;
+import com.example.buaadb.entity.Teacher;
 import com.example.buaadb.entity.User;
 import com.example.buaadb.exception.ServiceException;
+import com.example.buaadb.function.InExport;
 import com.example.buaadb.function.PageDivision;
 import com.example.buaadb.function.TokenUtils;
 import com.example.buaadb.mapper.ClassMapper;
@@ -15,6 +18,10 @@ import com.example.buaadb.service.ClassService;
 import com.example.buaadb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -59,5 +66,10 @@ public class UserController {
             queryWrapper.eq("permission", permission);
         }
         return Result.success(PageDivision.getPage(userService.list(queryWrapper), pageNum, pageSize));
+    }
+
+    @GetMapping("/exportlog")
+    public void export(HttpServletResponse response) throws IOException {
+        userService.export(response);
     }
 }
