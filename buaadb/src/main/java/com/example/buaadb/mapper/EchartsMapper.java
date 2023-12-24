40402e785ca03c0446cc824c882a94153d7755fe
1 <21371202@buaa.edu.cn>
2023-12-24 23:37:37 +0800

    123123

--- buaadb/src/main/java/com/example/buaadb/mapper/EchartsMapper.java
+++ buaadb/src/main/java/com/example/buaadb/mapper/EchartsMapper.java
@@ -9,9 +9,9 @@ import java.util.List;
 
 @Mapper
 public interface EchartsMapper {
-    @Select("SELECT rate AS name, COUNT(rate) AS value FROM (SELECT *, rate(Segrade) AS rate FROM Sel WHERE cno = ${cno}) AS board GROUP BY rate")
+    @Select("SELECT rate AS name, COUNT(rate) AS value FROM (SELECT *, rate(Segrade) AS rate FROM Sel WHERE cno = #{cno}) AS board GROUP BY rate")
     List<RateInfo> getRateCno(@Param("cno") String cno);
-    @Select("SELECT rate AS name, COUNT(rate) AS value FROM (SELECT *, rate(Segrade) AS rate FROM Sel WHERE sno = ${sno}) AS board GROUP BY rate")
+    @Select("SELECT rate AS name, COUNT(rate) AS value FROM (SELECT *, rate(Segrade) AS rate FROM Sel WHERE sno = #{sno}) AS board GROUP BY rate")
     List<RateInfo> getRateSno(@Param("cno") String sno);
 
 }
